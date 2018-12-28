d11439987560c21162582e7b09bf28af6e371c0f
Brandon Teich <brandonteich123@gmail.com>
2018-12-28 20:04:40 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -14,10 +14,8 @@ class MusicLibraryController
        puts "To play a song, enter 'play song'."
        puts "To quit, type 'exit'."
        puts "What would you like to do?"
- 
-        input = gets.strip
- 
-        case input
+       input = gets.strip
+       case input
          when "list songs"
            list_songs
          when "list artists"
@@ -33,25 +31,22 @@ class MusicLibraryController
         end
      end
    end
- 
-    def list_songs
+  def list_songs
      Song.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |s, i|
        puts "#{i}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
      end
    end
- 
-    def list_artists
+  def list_artists
      Artist.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |a, i|
        puts "#{i}. #{a.name}"
      end
-   end
- 
-    def list_genres
-     Genre.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |g, i|
-       puts "#{i}. #{g.name}"
+  end
+  def list_genres
+  Genre.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |g, i|
+      puts "#{i}. #{g.name}"
      end
    end
- 
+=    
     def list_songs_by_artist
      puts "Please enter the name of an artist:"
      input = gets.strip
