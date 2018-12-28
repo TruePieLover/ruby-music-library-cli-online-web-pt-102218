742c095f12465ba4f14f66407c02d7bde3352c69
Brandon Teich <brandonteich123@gmail.com>
2018-12-28 20:05:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,50 +1,39 @@
 class Song
- 
-    attr_accessor :name
-   attr_reader :artist, :genre
+  attr_accessor :name
+  attr_reader :artist, :genre
    @@all = []
- 
-    def initialize(name, artist = nil, genre = nil)
+   def initialize(name, artist = nil, genre = nil)
      @name = name
      self.artist = artist if artist
      self.genre = genre if genre
    end
- 
-    def artist=(artist)
+   def artist=(artist)
      @artist = artist
      artist.add_song(self)
    end
- 
-    def genre=(genre)
+   def genre=(genre)
      @genre = genre
      genre.songs << self unless genre.songs.include?(self)
    end
- 
-    def self.all
+   def self.all
      @@all
    end
- 
-    def self.destroy_all
+   def self.destroy_all
      @@all.clear
    end
- 
-    def save
+   def save
      self.class.all << self
    end
- 
-    def self.create (song_name)
-     self.new(song_name).tap{|s| s.save}
+  def self.create (song_name)
+    self.new(song_name).tap{|s| s.save}
    end
- 
-    def self.find_by_name(name)
-     self.all.find {|a| a.name == name}
+  def self.find_by_name(name)
+    self.all.find {|a| a.name == name}
+  end
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create(name)
    end
- 
-    def self.find_or_create_by_name(name)
-     self.find_by_name(name) || self.create(name)
-   end
- 
-    def self.new_from_filename(filename)
+def self.new_from_filename(filename)
      parts = filename.split(" - ")
      artist_name, song_name, genre_name = parts[0], parts[1], parts[2].gsub(".mp3", "")
  
